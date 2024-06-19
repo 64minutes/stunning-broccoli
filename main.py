50255c74aa420be679a54af067a1b6af19808b31
HanWen <sphw6j@hotmail.com>
2024-06-19 12:44:20 +0800

    This is HW modifying your code.

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
-def say_hello(user="world"):
-  print(f"Hello, {user}")
+def say_hello(user="world", occupation = "Jobless"):
+  print(f"Hello, {user}, my occupation is {occupation}.")
 
 
 def get_name():
-  return input("What is your name? \n")
+  return input("What is your name?\n")
+
+def get_occupation():
+  return input("What is your occupation?\n")
 
 
 if __name__ == "__main__":
-  say_hello(get_name())
+  say_hello(get_name(), get_occupation())
